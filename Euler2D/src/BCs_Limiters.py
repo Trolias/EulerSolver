c36af8f5a86b032d5053a4f2b648098818cd552d
Trolias <107854420+Trolias@users.noreply.github.com>
2026-03-02 22:14:31 +0200

    Add files via upload

--- Euler2D/src/BCs_Limiters.py
+++ Euler2D/src/BCs_Limiters.py
@@ -1,49 +1,22 @@
 import jax 
 import jax.numpy as jnp
+import params
 
 def limiter(cell, name = "Minmod"):
     # Define smoothness_indicator " $r_{\pm{\frac{1}{2}}}$ "
-    r_plus_half_x = (cell[:, 2:, :] - cell[:, 1:-1, :]) / (cell[:, 1:-1, :] - cell[:, :-2, :] + 1e-08)
-    r_minus_half_x = (cell[:, 1:-1, :] - cell[:, :-2, :]) / (cell[:, 2:, :] - cell[:, 1:-1, :] + 1e-08)
+    r_plus_half = (cell[2:, :] - cell[1:-1, :]) / (cell[1:-1, :] - cell[:-2, :] + 1e-08)
+    r_minus_half = (cell[1:-1, :] - cell[:-2, :]) / (cell[2:, :] - cell[1:-1, :] + 1e-08)
 
-    r_plus_half_y = (cell[2:, :, :] - cell[1:-1, :, :]) / (cell[1:-1, :, :] - cell[:-2, :, :] + 1e-08)
-    r_minus_half_y = (cell[1:-1, :, :] - cell[:-2, :, :]) / (cell[2:, :, :] - cell[1:-1, :, :] + 1e-08)
+    psi_r = jnp.maximum(0.0, jnp.minimum(1.0, r_plus_half))
+    psi_l = jnp.maximum(0.0, jnp.minimum(1.0, r_minus_half))
 
-
-    psi_r = jnp.maximum(0.0, jnp.minimum(1.0, r_plus_half_x))
-    psi_l = jnp.maximum(0.0, jnp.minimum(1.0, r_minus_half_x))
-    psi_t = jnp.maximum(0.0, jnp.minimum(1.0, r_plus_half_y))
-    psi_b = jnp.maximum(0.0, jnp.minimum(1.0, r_minus_half_y))
-
-    return psi_l, psi_r, psi_t, psi_b
+    return psi_l, psi_r
 
 
-def Boundary_Conditions(cell, CDL, CDR, CDT, CDB, name = "Constant"):
+def Boundary_Conditions(cell, CDL, CDR, name = "Transmissive", ng = params.gc):
 
-    if name == "Constant":
-        cell = cell.at[:, 0, :].set(cell[:, 1, :])
-        cell = cell.at[:, -1, :].set(cell[:, -2, :])
-        cell = cell.at[0, :, :].set(cell[1, :, :])
-        cell = cell.at[-1, :, :].set(cell[-2, :, :])
+    if name == "Transmissive":
+        cell = cell.at[:ng, :].set(cell[ng, :])
+        cell = cell.at[-ng:, :].set(cell[-ng-1, :])
 
-        CDL = CDL.at[:,0,:].set(cell[:,0,:])
-        CDR = CDR.at[:,0,:].set(cell[:,1,:])
-        CDL = CDL.at[0,:,:].set(cell[0,:-1,:])
-        CDR = CDR.at[0,:,:].set(cell[1,1:,:])
-
-        CDL = CDL.at[:,-1,:].set(cell[:,-2,:])
-        CDR = CDR.at[:,-1,:].set(cell[:,-1,:])
-        CDL = CDL.at[-1,:,:].set(cell[-2,:-1,:])
-        CDR = CDR.at[-1,:,:].set(cell[-1,1:,:])
-
-        CDT = CDT.at[0,:,:].set(cell[0,:,:])
-        CDB = CDB.at[0,:,:].set(cell[1,:,:])
-        CDT = CDT.at[:,0,:].set(cell[1:,0,:])
-        CDB = CDB.at[:,0,:].set(cell[:-1,1,:])
-        
-        CDT = CDT.at[-1,:,:].set(cell[-2,:,:])
-        CDB = CDB.at[-1,:,:].set(cell[-1,:,:])
-        CDT = CDT.at[:,-1,:].set(cell[1:,-2,:])
-        CDB = CDB.at[:,-1,:].set(cell[:-1,-1,:])
-
-    return cell, CDL, CDR, CDT, CDB
+    return cell, CDL, CDR
--- Euler2D/src/main.py
+++ Euler2D/src/main.py
@@ -8,65 +8,50 @@ from helpers import*
 
 
 # Creating fields
-x = jnp.linspace(params.Lxstart, params.Lxend, params.Nx)
-y = jnp.linspace(params.Lystart, params.Lyend, params.Ny)
-U = jnp.zeros((params.Ny, params.Nx, 4))
-CSV_L = jnp.zeros((params.Ny, params.Nx-1, 4))
+ng = params.gc # Number of ghost nodes
+x = params.Lstart + (jnp.arange(params.N) + 0.5)*params.dx
+U = jnp.zeros((params.N + 2*ng ,3))
+CSV_L = jnp.zeros((params.N + 2*ng-1, 3))
 CSV_R = jnp.zeros_like(CSV_L)
-CSV_T = jnp.zeros((params.Ny-1, params.Nx, 4))
-CSV_B = jnp.zeros_like(CSV_T)
-
 
 # Initialising Fields
 rho = 0.0
 u = 0.0
 p = 0.0
 
-X, Y = jnp.meshgrid(x, y)
+for i in range(params.N+ng):
+    if (params.cas == 1):
 
-if params.cas == 1:
-    rho = jnp.where((X<=0.0) & (Y>=0.0), 0.5323,
-        jnp.where((X>0.0) & (Y>0.0), 1.5,
-        jnp.where((X<=0.0) & (Y<0.0), 0.138,
-                    0.5323)))
-    
-    u = jnp.where((X<=0.0) & (Y>=0.0), 1.206,
-    jnp.where((X>0.0) & (Y>0.0), 0.0,
-    jnp.where((X<=0.0) & (Y<0.0), 1.206,
-              0.0)))  
+        if x[i] <= 0.5:  
+            rho = 1.0
+            u =  0.0
+            p = 1.0
+        else:
+            rho = 0.125
+            u = 0.0
+            p = 0.1
 
-    v = jnp.where((X<=0.0) & (Y>=0.0), 0.0,
-        jnp.where((X>0.0) & (Y>0.0), 0.0,
-        jnp.where((X<=0.0) & (Y<0.0), 1.206,
-                1.206))) 
+    elif (params.cas == 2):
 
-    p = jnp.where((X<=0.0) & (Y>=0.0), 0.3,
-        jnp.where((X>0.0) & (Y>0.0), 1.5,
-        jnp.where((X<=0.0) & (Y<0.0), 0.029,
-                0.3)))
+        if x[i] < -4.0:   
+            rho = 3.857143
+            u =  2.629369
+            p = 10.3333
+        else:
+            rho = 1.0 + 0.2*jnp.sin(5.0*x[i])
+            u = 0.0
+            p = 1.0
 
-elif params.cas == 2:
-    rho = jnp.where((X<0.5), 1.0, 0.125)
-    u = jnp.where((X<0.5), 0.0, 0.0)
-    v = jnp.where((X<0.5), 0.0, 0.0)
-    p = jnp.where((X<0.5), 1.0, 0.1)
-elif params.cas == 3:
-    radius = jnp.sqrt(X**2 + Y**2)
-    rho = jnp.where(radius < 0.2, 1.0, 0.125)
-    u = jnp.where(radius < 0.2, 0.0, 0.0)
-    v = jnp.where(radius < 0.2, 0.0, 0.0)
-    p = jnp.where(radius < 0.2, 1.0, 0.1)
+    U = U.at[i, 0].set(rho)
+    U = U.at[i, 1].set(rho*u)
+    U = U.at[i, 2].set( p/(params.gamma-1) + 0.5*rho*u**2)
 
 
+U, CSV_L, CSV_R = Boundary_Conditions(U, CSV_L, CSV_R)    # !!! This line is IMPORTANT !!!
+# PV = calc_PV(U[ng:-ng,:])
+# plt.plot(x,PV[:,0])
+# plt.show()
 
-
-U = U.at[:, :, 0].set(rho)
-U = U.at[:, :, 1].set(rho*u)
-U = U.at[:, :, 2].set(rho*v)
-U = U.at[:, :, 3].set( p/(params.gamma-1) + 0.5*rho*(u**2 + v**2))
-
-plt.contourf(X, Y, U[:,:,0])
-plt.show()
 
 t = 0.0
 while (t < params.t_end):
@@ -76,24 +61,21 @@ while (t < params.t_end):
     # Calculate Primite Varibaels (PV)
     PV = calc_PV(Uold)
     # Compute suitable time_step based on maximum speed
-    dt = calc_dt(Uold, PV[:,:,3])
+    dt = calc_dt(Uold[ng:-ng], PV[ng:-ng,2])
     dt = jnp.minimum(dt, params.t_end - t)
     # jax.debug.print("dt = : {d_t}", d_t = dt)
 
     # Flux Reconstruction plus flux Calculation through Riemann solvers
-    Uold, U_L, U_R, U_T, U_B, Intercell_Flux_x, Intercell_Flux_y = Reconst_plus_Riemann(Uold, CSV_L, CSV_R, CSV_T, CSV_B)
+    Uold, U_L, U_R, Intercell_Flux = Reconst_plus_Riemann(Uold, CSV_L, CSV_R)
     
     # Update Solution
-    U_interior = solve(Uold, U_L, U_R, U_T, U_B, Intercell_Flux_x, Intercell_Flux_y, dt, params.time_integrators[params.tim])
-    U = U.at[1:-1,1:-1,:].set(U_interior)
+    U_interior = solve(Uold, U_L, U_R, Intercell_Flux, dt, params.time_integrators[params.tim])
+    U = Uold.at[ng:-ng,:].set(U_interior)
     
     # Update time 
     t = t + dt
 
-PV = calc_PV(U)
-plt.contourf(X, Y, PV[:,:,0])
-# plt.plot(x, PV[params.Ny//2, :, 0])
-plt.show()
+PV = calc_PV(U[ng:-ng,:])
 
 # plt.plot(x, PV[:,0], color = 'magenta')
 # plt.xlim(params.Lstart, params.Lend)
@@ -102,7 +84,7 @@ plt.show()
 
 # plt.show()
 
-save_tecplot_dat("2DRieman801.dat", x, y, PV)
+save_dat("ExactSod.dat", x, PV)
 
-# plot_compare("HLLCWENO5001.dat", "Rus_WENO5_Char_Sod101.dat",
-#              label_a="Exact", label_b="WENO_char")
+# plot_compare("Exact.dat","DebugWENO.dat",
+#              label_a="Exact", label_b="HLL")
